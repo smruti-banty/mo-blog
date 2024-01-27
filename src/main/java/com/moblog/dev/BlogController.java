6482bb85db71b2017e994f23c1fb7fe6ac6f5365
jt-javatechnocrat <jt@javatechnocrat.com>
2024-01-27 07:44:19 +0530

    controller design add

---
+++ src/main/java/com/moblog/dev/BlogController.java
@@ -0,0 +1,22 @@
+package com.moblog.dev;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping({ "/", "/blogs" })
+    public String blogs() {
+        return "blogs";
+    }
+    
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/add-blog")
+    public String addBlog() {
+        return "add-blog";
+    }
+}
