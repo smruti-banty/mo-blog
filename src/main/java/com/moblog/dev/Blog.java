6518afd6aa93e5baa6dd0b62cabe8518fdb94d28
jt-javatechnocrat <jt@javatechnocrat.com>
2024-01-29 08:44:23 +0530

    backend logic implements

---
+++ src/main/java/com/moblog/dev/Blog.java
@@ -0,0 +1,9 @@
+package com.moblog.dev;
+
+import lombok.Data;
+
+@Data
+public class Blog {
+    private String heading;
+    private String description;
+}
--- src/main/java/com/moblog/dev/BlogController.java
+++ src/main/java/com/moblog/dev/BlogController.java
@@ -1,22 +1,44 @@
 package com.moblog.dev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
+    private final List<Blog> blogs;
+
+    public BlogController() {
+        blogs = new ArrayList<>();
+    }
+
     @GetMapping({ "/", "/blogs" })
-    public String blogs() {
+    public String blogs(Model model) {
+        model.addAttribute("blogs", blogs);
         return "blogs";
     }
 
     @GetMapping("/blog")
-    public String blog() {
+    public String blog(@RequestParam int id, Model model) {
+        var blog = blogs.get(id);
+        model.addAttribute("blog", blog);
         return "blog";
     }
 
     @GetMapping("/add-blog")
     public String addBlog() {
         return "add-blog";
+    }
+
+    @PostMapping("/add-blog")
+    public String addBlog(@ModelAttribute Blog blog) {
+        blogs.add(blog);
+        return "redirect:/blogs";
     }
 }
