69cbed85b2b9fed9a446b7a18432cf5b4268630f
jt-javatechnocrat <jt@javatechnocrat.com>
2024-02-02 08:47:46 +0530

    database add

--- src/main/java/com/moblog/dev/Blog.java
+++ src/main/java/com/moblog/dev/Blog.java
@@ -1,9 +1,14 @@
 package com.moblog.dev;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Blog {
+    private int id;
     private String heading;
     private String description;
 }
--- src/main/java/com/moblog/dev/service/BlogService.java
+++ src/main/java/com/moblog/dev/service/BlogService.java
@@ -3,6 +3,9 @@ package com.moblog.dev.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import com.moblog.dev.Blog;
@@ -12,17 +15,32 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BlogService {
-    private final ArrayList<Blog> blogs;
+    private final JdbcTemplate jdbcTemplate;
 
     public void addBlog(Blog blog) {
-        blogs.add(blog);
+        String sql = "INSERT INTO moblog (heading, description) VALUES (?,?)";
+        jdbcTemplate.update(sql, blog.getHeading(), blog.getDescription());
     }
 
     public List<Blog> getBlogs() {
-        return blogs;
+        String sql = "SELECT * FROM moblog";
+
+        RowMapper<Blog> rowMapper = (resultSet, rowNum) -> new Blog(
+                resultSet.getInt("id"),
+                resultSet.getString("heading"),
+                resultSet.getString("description"));
+
+        return jdbcTemplate.query(sql, rowMapper);
+        // return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Blog>());
     }
 
-    public Blog getBlog(int index) {
-        return blogs.get(index);
+    public Blog getBlog(int id) {
+        String sql = "SELECT * FROM moblog WHERE id = ?";
+        RowMapper<Blog> rowMapper = (resultSet, rowNum) -> new Blog(
+            resultSet.getInt("id"),
+            resultSet.getString("heading"),
+            resultSet.getString("description"));
+
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 }
