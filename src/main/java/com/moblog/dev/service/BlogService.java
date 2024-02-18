f2451ebbe1975a301139b669fc913efdd1d7603b
jt-javatechnocrat <jt@javatechnocrat.com>
2024-02-18 09:03:32 +0530

    refactor: external config

--- src/main/java/com/moblog/dev/service/BlogService.java
+++ src/main/java/com/moblog/dev/service/BlogService.java
@@ -3,6 +3,7 @@ package com.moblog.dev.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -17,9 +18,14 @@ import lombok.RequiredArgsConstructor;
 public class BlogService {
     private final JdbcTemplate jdbcTemplate;
 
+    @Value("${prefix}")
+    private String prefix;
+
     public void addBlog(Blog blog) {
+        var heading = prefix + " " + blog.getHeading();
+
         String sql = "INSERT INTO moblog (heading, description) VALUES (?,?)";
-        jdbcTemplate.update(sql, blog.getHeading(), blog.getDescription());
+        jdbcTemplate.update(sql, heading, blog.getDescription());
     }
 
     public List<Blog> getBlogs() {
@@ -37,9 +43,9 @@ public class BlogService {
     public Blog getBlog(int id) {
         String sql = "SELECT * FROM moblog WHERE id = ?";
         RowMapper<Blog> rowMapper = (resultSet, rowNum) -> new Blog(
-            resultSet.getInt("id"),
-            resultSet.getString("heading"),
-            resultSet.getString("description"));
+                resultSet.getInt("id"),
+                resultSet.getString("heading"),
+                resultSet.getString("description"));
 
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
